18bd5de498417ba4dee2b888b29c383ce36b25cb
qalibqurbanov <66569047+qalibqurbanov@users.noreply.github.com>
2022-08-29 00:43:01 +0400

    Add files via upload

---
+++ SCartDownloader_FormUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCartDownloader_FormUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /* Mutex-e ad vermek onu computer-wide edir, burada unikal ad vermeliyik:*/
+            using (Mutex mutex = new Mutex(false, "SoundCloud Artwork Downloader"))
+            {
+                /* 2 saniye gozle, eger proqramin bawqa bir instance-i artiq baglanmaq erefesindedirse: */
+                if (!mutex.WaitOne(TimeSpan.FromSeconds(1), false))
+                {
+                    MessageBox.Show("Another instance of this application is already running. Application will now close.", "SoundCloud Artwork Downloader", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new FormMain());
+            }
+        }
+    }
+}
